301062cb0030ce490fbb1ead4833844dff3ab54e
AlejandroS <3720366@alu.murciaeduca.es>
2025-11-11 09:43:31 +0100

    Varias vistas con dopetrope

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,19 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('/users', function () {
+    return view('users.usersList', [
+        'users' => [
+            ['id' => 1, "name" => 'Ana'],
+            ['id' => 2, "name" => 'Maria'],
+            ['id' => 3, "name" => 'Juan'],
+            ['id' => 4, "name" => 'Alejandro'],
+        ]
+
+    ]);
+});
+
+
 // ----------------------------------------
 Route::get('login', function () {
     return "Login usuario";
