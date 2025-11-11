e32dea28c6731a1bef3fbb97d0499c02a9f97000
AlejandroS <3720366@alu.murciaeduca.es>
2025-11-11 08:56:52 +0100

    Dopetrope Adaptado a Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('dopetrope.master')
+
+@section('menu')
+    @parent
+    <p>¡Hola {{ $nombre ?? 'colega' }}!</p>
+    @endsection
+
+@section('content')
+    <ul>
+        @if (count($users) === 1)
+            <li>Solo hay un usuario!</li>
+        @elseif (count($users)>1)
+            <li>Hay muchos usuarios!</li>
+            @include('users.usersList', ['users' => $users])
+        @else
+            <li>No hay ningun usuario :(</li>
+        @endif
+
+
+    </ul>
+@endsection
