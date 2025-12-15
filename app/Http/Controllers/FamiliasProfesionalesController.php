9a36daccc2206bef4e6192bfd0e5fcaf0b849533
AlejandroS <3720366@alu.murciaeduca.es>
2025-12-15 09:16:46 +0100

    preparando familias profesionales

---
+++ app/Http/Controllers/FamiliasProfesionalesController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FamiliaProfesional;
+use Illuminate\Http\Request;
+
+class FamiliasProfesionalesController extends Controller
+{
+    public function getIndex()
+    {
+        $familias_profesionales = FamiliaProfesional::all();
+        return view('familias-profesionales.index')
+            ->with('familias_profesionales', $familias_profesionales);
+    }
+
+    public function getShow($id)
+    {
+        $familia_profesional = FamiliaProfesional::findOrFail($id);
+        return view('familias-profesionales.show')
+            ->with('familia_profesional', $familia_profesional);
+    }
+
+    public function getCreate()
+    {
+        return view('familias-profesionales.create');
+    }
+
+    public function getEdit($id)
+    {
+        $familia_profesional = FamiliaProfesional::findOrFail($id);
+        return view('familias-profesionales.edit')
+            ->with('familia_profesional', $familia_profesional);
+    }
+
+    public function postCreate(Request $request)
+    {
+        $familiaProfesional = FamiliaProfesional::create($request->all());
+        return redirect()->action([self::class, 'getShow'], ['id' => $familiaProfesional->id]);
+    }
+
+
+
+    public function putCreate(Request $request, $id)
+    {
+        $familiaProfesional = FamiliaProfesional::findOrFail($id);
+
+        $familiaProfesional->update($request->all());
+        return redirect()->action([self::class, 'getShow'], ['id' => $familiaProfesional->id]);
+    }
+
+}
---
+++ resources/views/familias-profesionales/show.blade.php
@@ -0,0 +1,37 @@
+@extends('dopetrope.master')
+    @section('content')
+    <div class="row m-4">
+
+        <div class="col-sm-4">
+
+            <img src='/landed/images/logo.png' style="height:200px"/>
+
+        </div>
+        <div class="col-sm-8">
+
+            <h3><strong>Nombre: </strong>{{ $familia_profesional -> nombre }}</h3>
+            <h4><strong>Dominio: </strong>
+                <a href="http://github.com/2DAW-CarlosIII/{{ $familia_profesional -> codigo }}">
+                    http://github.com/2DAW-CarlosIII/{{ $familia_profesional -> codigo }}
+                </a>
+            </h4>
+            <h4><strong>Codigo: </strong>{{ $familia_profesional -> codigo }}</h4>
+            <p><strong>Nombre: </strong>{{ $familia_profesional -> nombre }}</p>
+
+
+@auth
+            <a class="btn btn-warning" href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getEdit'], ['id' => $familia_profesional->id]) }}">
+                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                Editar familia profesional.
+            </a>
+
+@endauth
+
+            <a class="btn btn-outline-info" href="{{ action([App\Http\Controllers\FamiliasProfesionalesController::class, 'getIndex']) }}">
+                Volver al listado
+            </a>
+
+
+        </div>
+    </div>
+    @endsection
